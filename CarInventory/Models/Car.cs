f1bb5264ba26816d76a712c3805a84306439c946
Nate Hathaway <natehathaway@byui.edu>
2025-11-22 11:24:48 -0700

    added edit car function, ability to handle image uploads, sorting options, and validations for car information

--- CarInventory/Models/Car.cs
+++ CarInventory/Models/Car.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CarInventory.Models
 {
     public class Car
     {
         public int Id { get; set; }
+        [Required]
         public string Make { get; set; }
+        [Required]
         public string Model { get; set; }
+        [Range(1900, 2030)]
         public int Year { get; set; }
+        [Range(1, 200000)]
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+        public string? ImageFileName { get; set; }
     }
 }
